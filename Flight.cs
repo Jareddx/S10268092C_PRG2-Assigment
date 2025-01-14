0ff22ae5e34dc0fff012b68c74336ca411181553
Jareddx <S10268092@connect.np.edu.sg>
2025-01-15 00:10:45 +0800

    added flight class

---
+++ Flight.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268092_PRG2Assigment
+{
+    abstract class Flight
+    {
+        private string flightNumber;
+
+        public string FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+        private string origin;
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        private string destination;
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+
+        private DateTime expectedTime;
+
+        public DateTime ExpectedTime
+        {
+            get { return expectedTime; }
+            set { expectedTime = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Flight(string fn, string o, string d, string et, string s)
+        {
+            FlightNumber = fn;
+            Origin = o;
+            Destination = d;
+            ExpectedTime = Convert.ToDateTime(et);
+            Status = s;
+
+        }
+        public abstract double CalculateFees();
+
+        public override string ToString()
+        {
+            return $"Flight Number: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, " +
+            $"Expected Time: {ExpectedTime:yyyy-MM-dd HH:mm}, Status: {Status}";
+        }
+
+    }
+}
+
