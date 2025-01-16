24bd9c02ac4377ddb98283c2138b52244f041b9a
Jareddx <S10268092@connect.np.edu.sg>
2025-01-17 01:21:57 +0800

    Added Basic features qn 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using S10268092_PRG2Assigment;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("sigma");
+
+
+// Basic Features Qn 2
+IDictionary<string, Flight> FlightCollection = new Dictionary<string, Flight>();
+
+using (StreamReader sr = new StreamReader("flights.csv"))
+{
+    string? s = sr.ReadLine();
+    while ((s = sr.ReadLine()) != null)
+    {
+        string[] data = s.Split(',');
+        NORMFlight flightdata = new NORMFlight(data[0], data[1], data[2], data[3], data[4]);
+        FlightCollection.Add(data[0], flightdata);
+    }
+}
