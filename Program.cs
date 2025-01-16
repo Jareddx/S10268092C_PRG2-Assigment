76db1e0ec1a60fd5b1cd6c58a057c2bd19b76baf
YJun-Jie          git config --global user.email <S10268048@connect.np.edu.sg>
2025-01-17 01:22:54 +0800

    test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("sigma");
+﻿using S10268092_PRG2Assigment;
+using System;
+using System.Collections.Generic;
+
+using (StreamReader sr = new StreamReader("airlines.csv"))
+{
+    string? s = sr.ReadLine(); 
+
+    while ((s = sr.ReadLine()) != null)
+    {
+        string[] line = s.Split(',');
+        
+
+        Console.WriteLine(s);
+    }
+}
+
+using (StreamReader sr = new StreamReader("boardinggates.csv"))
+{
+    string? s = sr.ReadLine();
+
+    while ((s = sr.ReadLine()) != null)
+    {
+
+
+        Console.WriteLine(s);
+    }
+}
