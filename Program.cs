b22c7087e829dc175b0e19eb44510a315cf36ab5
Jareddx <S10268092@connect.np.edu.sg>
2025-01-22 03:35:03 +0800

    Added displaytable and finished basic features 3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 using System.Collections.Generic;
 using S10268092_PRG2Assigment;
 
+void DisplayTable()
+{
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("=============================================");
+    Console.WriteLine("1. List All Flights");
+    Console.WriteLine("2. List Boarding Gates");
+    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+    Console.WriteLine("4. Create Flight");
+    Console.WriteLine("5. Display Airline Flights");
+    Console.WriteLine("6. Modify Flight Details");
+    Console.WriteLine("7. Display Flight Schedule");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+    Console.Write("Please select your option: ");
+
+    int option = Convert.ToInt16(Console.ReadLine());
+    if (option == 1)
+    {
+        DisplayFlightInfo();
+        Console.WriteLine();
+        DisplayTable();
+    }
+}
+
 // Basic Features Qn 1
 using (StreamReader sr = new StreamReader("airlines.csv"))
 {
@@ -112,18 +137,62 @@ using (StreamReader sr = new StreamReader("flights.csv"))
 void DisplayFlightInfo()
 {
     List<Flight> FlightList = new List<Flight>();
+    List<Airline> temp = new List<Airline>();
+    String AirlineName = "";
+
+    using (StreamReader sr = new StreamReader("airlines.csv"))
+    {
+        string? s = sr.ReadLine();
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] data = s.Split(',');
+            Airline AirlineData = new Airline()
+            {
+                Name = data[0],
+                Code = data[1],
+            };
+            temp.Add(AirlineData);
+        }
+    }
 
     foreach (var flight in FlightCollection)
     {
         Flight flightdata = flight.Value;
         FlightList.Add(flightdata);
     }
-
+    Console.WriteLine("Flight Number   Airline Name           Origin                 Destination            Expected Departure/Arrival Time");
     for (int i = 0; i < FlightList.Count(); i++)
     {
         Flight F = FlightList[i];
         string[] split = F.FlightNumber.Split(" ");
-        
+
+        for (int x = 0; x < temp.Count(); x++)
+        {
+            Airline C = temp[x];
+            if (C.Code == split[0])
+            {
+                AirlineName = C.Name;
+            }
+        }
+        Console.WriteLine("{0,-16}{1,-23}{2,-23}{3,-23}{4,-10}", F.FlightNumber, AirlineName, F.Origin, F.Destination, F.ExpectedTime);
     }
 }
 
+// Basic Features Qn 4
+
+// Basic Features Qn 5
+
+// Basic Features Qn 6
+
+// Basic Features Qn 7
+
+// Basic Features Qn 8
+
+// Basic Features Qn 9
+
+// Main Loop
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+DisplayTable();
